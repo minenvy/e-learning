9ba7e4f5fc31b4cd466aa9af1fb555d685eda981
MinhNV <minhnv@bravebits.vn>
2023-09-03 13:43:29 +0700

    add dicionary

--- app/(pages)/(root)/writing/page.tsx
+++ app/(pages)/(root)/writing/page.tsx
@@ -1,7 +1,38 @@
+'use client'
+
+import { EditorState, convertToRaw } from 'draft-js'
+import draftToHtml from 'draftjs-to-html'
+import htmlToDraft from 'html-to-draftjs'
+import dynamic from 'next/dynamic'
+import { useState } from 'react'
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css'
+
+const Editor = dynamic(
+  () => import('react-draft-wysiwyg').then((mod) => mod.Editor),
+  { ssr: false }
+)
+
 export default function General() {
-	return (
-		<main>
-			<p>General</p>
-		</main>
-	)
+  const [editorState, setEditorState] = useState(EditorState.createEmpty())
+
+  const onEditorStateChange = (state: EditorState) => {
+    setEditorState(state)
+  }
+
+  return (
+    <div>
+      <div>
+        <Editor
+          editorState={editorState}
+          wrapperClassName="demo-wrapper"
+          editorClassName="demo-editor"
+          onEditorStateChange={onEditorStateChange}
+        />
+        <textarea
+          disabled
+          value={draftToHtml(convertToRaw(editorState.getCurrentContent()))}
+        />
+      </div>
+    </div>
+  )
 }
